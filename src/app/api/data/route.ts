a8b4f67699001c00f59ef099c25edef62a7665ec
Jyoshna Korada <jyoshna.korada@pal.tech>
2025-06-12 18:11:39 +0530

    initial

---
+++ src/app/api/data/route.ts
@@ -0,0 +1,33 @@
+// pages/api/data.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+import { queryDatabase } from "../../../lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   try {
+//     const rows = await queryDatabase("SELECT Name FROM [Config.Role]");
+//     res.status(200).json(rows);
+//   } catch (err) {
+//     res.status(500).json({ error: "Failed to fetch data" });
+//   }
+// }
+// export default function handler(req, res) {
+//   res.status(200).json({ message: "Hello from Next.js API!" });
+// }
+
+//req is short for request
+export async function GET(req: NextRequest) {
+  try {
+    const rows = await queryDatabase("SELECT Name FROM [Config.Role]");
+    return NextResponse.json(rows);
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/lib/db.ts
@@ -0,0 +1,28 @@
+// src/lib/db.ts
+import sql from "mssql";
+
+const config: sql.config = {
+  user: process.env.DB_USER!,
+  password: process.env.DB_PASS!,
+  server: process.env.DB_SERVER!,
+  port: parseInt(process.env.DB_PORT || "1433"),
+  database: process.env.DB_NAME!,
+  options: {
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+};
+
+export async function queryDatabase<T = any>(query: string): Promise<T[]> {
+  try {
+    console.log(query);
+    const pool = await sql.connect(config);
+    console.log(pool);
+    const result = await pool.request().query(query);
+    console.log(result);
+    return result.recordset;
+  } catch (error) {
+    console.error("SQL Error:", error);
+    throw error;
+  }
+}
